f75294e896bee6580ebbd0bf7f6096c11ca2e4a8
Lauro <lauropimentel12@gmail.com>
2024-10-28 19:24:23 +0000

    first commit

---
+++ src/middleware/not-found.ts
@@ -0,0 +1,7 @@
+
+import { Response, Request, NextFunction } from "express";
+import { CustomError } from "../lib/custom-error.ts";
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  return next(new CustomError("Route not found", 404));
+}
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { urlencoded, json } from "express";
+import { notFound } from "./middleware/not-found";
+import { error } from "./middleware/error";
+
+
+const app = express();
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+// ... other middlewares and routes ...
+
+app.use(notFound);
+app.use(error);
+
+export default app;
