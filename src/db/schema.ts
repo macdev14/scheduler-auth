3d0d15ebfabc5bc154df25b6ab64dbb9c819f719
Lauro <lauropimentel12@gmail.com>
2024-11-04 19:49:22 +0000

    fix server

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { text, varchar, pgTable, integer } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
+export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   username: varchar('username').unique(),
   password: text('password'),
